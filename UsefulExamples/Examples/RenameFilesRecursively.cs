cdc076ab3177d6dbbe6d895ee05d71af7db7ee11
Guilherme Marques <guilherme.marques@quartalfs.com>
2019-12-12 16:34:03 +0000

    Rename from portuguese to english, add solution

---
+++ UsefulExamples/Examples/RenameFilesRecursively.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace UsefulExamples.Examples
+{
+    public static class RenameFilesRecursively
+    {
+        public static void runDirectory(string currentDir, string[] subDirectory)
+        {
+            List<FileInfo> directoryFiles;
+            string[] directories;
+
+            directories = Directory.GetDirectories(currentDir);
+            foreach (string directory in directories)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+                directoryFiles = directoryInfo.GetFiles().OrderBy(x => x.LastWriteTime).ToList();
+
+                int count = 0;
+                foreach (FileInfo file1 in directoryFiles)
+                {
+                    try
+                    {
+                        string newName = count.ToString() + "_" + directoryInfo.Name + file1.Extension;
+                        string oldName = file1.Name;
+                        File.Move(file1.FullName, file1.FullName.Replace(oldName, newName));
+                    }
+                    catch(Exception ex) {
+                        string error = ex.Message;
+                    }
+                    count++;
+                }
+
+                runDirectory(directory, subDirectory);
+            }
+        }
+    }
+}
---
+++ UsefulExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using UsefulExamples.Examples;
+using System.IO;
+
+namespace UsefulExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp";
+            string[] subDirectories = Directory.GetDirectories(path);
+            RenameFilesRecursively.runDirectory(path, subDirectories);
+        }
+    }
+}
